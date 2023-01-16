ef9af8eb80c8cbfaf13975fdf2c5de7f0779fded
Leonardo Antezana <leonardoantezana59@gmail.com>
2023-01-16 15:39:10 -0300

    creando funcionalidad de los estados de los campos del formulario

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,17 @@ import Input from "./components/Input/Input"
 import { useState } from "react"
 
 function App() {
-  const [infoCard, setInfoCard] = useState({name: '', numbers: '', pin: '', date: ''})
+  const [nameCard, setNameCard] = useState({content: '', valid: true})
+  const [numberCard, setNumberCard] = useState({content:'', valid: true})
+  const [pinCard, setPinCard] = useState({content: '', valid: true})
+  const [date, setDate] =useState({day: '', year:''})
+  
+  const express = {
+    nombre: /^[a-zA-ZÀ-ÿ\s]/,
+    numbers: /^.{19,19}$/,
+    dia: /^\d{1,31}$/,
+    anio: /^\d{23,30}$/}
+
   return (
     <div className={styles.App}>
       <div className={styles.container}>
@@ -15,17 +25,55 @@ function App() {
           <Card className={styles.card__top} backgroundUrl={bgCardTop}>
             <div className={styles.container__image}><img src={icon} alt="logo-card" /></div>
             <div className={styles.info__card}>
-              <p className={styles.digits}>{infoCard.numbers}</p>
-              <p className={styles.info__card__bottom}><span>{infoCard.name}</span><span>{infoCard.date}</span></p>
+              <p className={styles.digits}>{numberCard.content}</p>
+              <p className={styles.info__card__bottom}><span>{nameCard.content}</span><span>{date.day}</span></p>
             </div>
           </Card>
           <Card className={styles.card__bottom} backgroundUrl={bgCardBottom}>
-            <p>{infoCard.pin}</p>
+            <p>{pinCard.content}</p>
           </Card>
         </div>
-        <div className={styles.form}>
-          <Input/>
-        </div>
+        <form className={styles.form}>
+          <Input labelText='cardholder name'
+          setFn={setNameCard}
+          type='text'
+          value={nameCard.content}
+          onChange={(e) => setNameCard(state => ({...state, content: e.target.value}))}
+          placeholder='Your name'
+          maxLength={30}
+          expReg={express.nombre}
+          />
+          <Input labelText='card number'
+          setFn={setNumberCard}
+          type='text'
+          value={numberCard.numbers}
+          onChange={(e) => setNumberCard(state => ({...state, content: e.target.value}))}
+          placeholder='Card Number'
+          maxLength={19}
+          expReg={express.numbers}
+          />
+          <Input labelText='exp.date (MM/YY)' 
+          type='text' 
+          value={date.day}
+          onChange={(e) => setDate(state => ({...state, day: e.target.value}))}
+          placeholder='Day'
+          maxLength={2} 
+          />
+          <Input type='text' 
+          value={date.year}
+          onChange={(e) => setDate(state => ({...state, year: e.target.value}))}
+          placeholder='YY'
+          maxLength={2} 
+          />
+          <Input labelText='cvc' 
+          type='text' 
+          value={pinCard.content}
+          onChange={(e) => setPinCard(state => ({...state, content: e.target.value}))}
+          placeholder='e.g 123'
+          maxLength={3} 
+          />
+          <button>Confirm</button>
+        </form>
       </div>      
     </div>
   )
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,7 +1,28 @@
 
-function Input() {
+function Input({ setFn, labelText, onChange, type, value, placeholder, maxLength, expReg }) {
+  const validation = () => {
+    if(expReg.test(value)){
+      setFn(state => ({...state, valid: true}))
+      console.log('es valido')
+    }
+    else{
+      setFn(state => ({...state, valid: false}))
+      console.log('es invalido')
+    }
+  }
   return (
-    <div>Input</div>
+    <>
+      <label>{labelText}</label>
+      <input 
+      type={type}
+      value={value}
+      onChange={onChange}
+      onKeyUp={validation}
+      placeholder={placeholder}
+      maxLength={maxLength}
+      required
+      />
+    </>
   )
 }
 
